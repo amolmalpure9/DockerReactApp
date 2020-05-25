7233ca13b52c725466b11fa0a4c8dc0cbff29a5f
amolmalpure9 <amolmalpure9@gmail.com>
2020-05-26 00:00:51 +0100

    test failed fixes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hi Dipali how are you
+        learn react
         </a>
       </header>
     </div>
